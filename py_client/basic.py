730a98a6f8345023ccc1b7b8c4929b0a9028f292
Kodeck James <kodeckjames095@gmail.com>
2024-07-16 13:22:28 +0300

    Fourth commit

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,4 +1,11 @@
 import requests
 
-endpoints = "https://httpbin.org/status/200"
-endpoints = "https://httpbin.org/anything"
+endpoint = "https://httpbin.org/status/200"
+endpoint = "https://httpbin.org/"
+
+
+get_response = requests.get(endpoint, json={"query":"Hello world"}) #HTTP Request
+print(get_response.text) #Prints raw text code
+# A regular HTTP Request gives you HTML
+# A REST API HTTP Request gives JSON
+# JSON => JavaScript Object Notation
