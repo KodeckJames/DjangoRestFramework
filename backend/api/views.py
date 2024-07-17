f6967fc8988452c3aec835e18a5fa91ab1758893
Kodeck James <kodeckjames095@gmail.com>
2024-07-17 17:18:27 +0300

    Sixth commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,9 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
+    body = request.body #byte string of JSON data
+    print(body)
     return JsonResponse({"message":"Hi there"})
